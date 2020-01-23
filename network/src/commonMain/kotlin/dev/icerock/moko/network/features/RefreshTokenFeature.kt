26d57e9e34787bd5645e5cfe616c1f0b3c3db186
Andrew Kovalev <kovalev@icerockdev.com>
2020-01-23 11:31:56 +0700

    #7 RefreshToken feature added

---
+++ network/src/commonMain/kotlin/dev/icerock/moko/network/features/RefreshTokenFeature.kt
@@ -0,0 +1,47 @@
+package dev.icerock.moko.network.features
+
+import io.ktor.client.HttpClient
+import io.ktor.client.HttpClientConfig
+import io.ktor.client.features.HttpClientFeature
+import io.ktor.client.request.HttpRequestBuilder
+import io.ktor.client.request.takeFrom
+import io.ktor.client.response.HttpReceivePipeline
+import io.ktor.http.HttpStatusCode
+import io.ktor.util.AttributeKey
+
+class RefreshTokenFeature(
+        private val updateTokenHandler: suspend () -> Boolean
+) {
+
+    class Config {
+        var updateTokenHandler: (suspend () -> Boolean)? = null
+
+        fun build() = RefreshTokenFeature(
+                updateTokenHandler ?: throw IllegalArgumentException("updateTokenHandler should be passed")
+        )
+    }
+
+    companion object Feature : HttpClientFeature<Config, RefreshTokenFeature> {
+
+        override val key = AttributeKey<RefreshTokenFeature>("RefreshTokenFeature")
+
+        override fun prepare(block: Config.() -> Unit) = Config().apply(block).build()
+
+        override fun install(feature: RefreshTokenFeature, scope: HttpClient) {
+            scope.receivePipeline.intercept(HttpReceivePipeline.After) {
+                if (context.response.status == HttpStatusCode.Unauthorized && feature.updateTokenHandler()) {
+                    val requestBuilder = HttpRequestBuilder().takeFrom(context.request)
+                    proceedWith(context.client.execute(requestBuilder).response)
+                } else {
+                    proceedWith(it)
+                }
+            }
+        }
+    }
+}
+
+fun HttpClientConfig<*>.install(tokenUpdater: suspend () -> Boolean) {
+    install(RefreshTokenFeature) {
+        this.updateTokenHandler = tokenUpdater
+    }
+}
