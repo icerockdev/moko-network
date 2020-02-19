8f25cabbb24a28138b0b4a5e73012aa338b56a96
Andrew Kovalev <kovalev@icerockdev.com>
2020-02-19 15:35:05 +0700

    #14 Simple language provider implemantation

---
+++ network/src/commonMain/kotlin/dev/icerock/moko/network/LanguageProvider.kt
@@ -0,0 +1,9 @@
+/*
+ * Copyright 2020 IceRock MAG Inc. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package dev.icerock.moko.network
+
+import dev.icerock.moko.network.features.LanguageFeature
+
+expect class LanguageProvider(): LanguageFeature.LanguageCodeProvider
