fffef00d136defea19966841f5741b8e8046d7de
tetraquark <avladislavv@gmail.com>
2020-07-20 17:51:17 +0700

    #24: add mutex to RefreshTokenFeature to fix parallel requests

--- network/src/commonMain/kotlin/dev/icerock/moko/network/features/RefreshTokenFeature.kt
+++ network/src/commonMain/kotlin/dev/icerock/moko/network/features/RefreshTokenFeature.kt
@@ -13,20 +13,26 @@ import io.ktor.client.statement.HttpReceivePipeline
 import io.ktor.client.statement.HttpResponse
 import io.ktor.http.HttpStatusCode
 import io.ktor.util.AttributeKey
+import kotlinx.coroutines.sync.Mutex
 
 class RefreshTokenFeature(
-    private val updateTokenHandler: suspend () -> Boolean
+    private val updateTokenHandler: suspend () -> Boolean,
+    private val getCurrentToken: () -> String
 ) {
 
     class Config {
         var updateTokenHandler: (suspend () -> Boolean)? = null
+        var getCurrentToken: (() -> String)? = null
 
         fun build() = RefreshTokenFeature(
-            updateTokenHandler ?: throw IllegalArgumentException("updateTokenHandler should be passed")
+            updateTokenHandler ?: throw IllegalArgumentException("updateTokenHandler should be passed"),
+            getCurrentToken ?: throw IllegalArgumentException("getCurrentToken should be passed")
         )
     }
 
     companion object Feature : HttpClientFeature<Config, RefreshTokenFeature> {
+
+        private val refreshTokenHttpFeatureMutex = Mutex()
 
         override val key = AttributeKey<RefreshTokenFeature>("RefreshTokenFeature")
 
@@ -34,10 +40,33 @@ class RefreshTokenFeature(
 
         override fun install(feature: RefreshTokenFeature, scope: HttpClient) {
             scope.receivePipeline.intercept(HttpReceivePipeline.After) {
-                if (context.response.status == HttpStatusCode.Unauthorized && feature.updateTokenHandler()) {
-                    val requestBuilder = HttpRequestBuilder().takeFrom(context.request)
-                    val result: HttpResponse = context.client.request(requestBuilder)
-                    proceedWith(result)
+                if (context.response.status == HttpStatusCode.Unauthorized) {
+                    val currentToken = feature.getCurrentToken()
+
+                    refreshTokenHttpFeatureMutex.lock()
+
+                    // If the local current token is already changed, then the feature will
+                    // repeat the previous request
+                    if (currentToken != feature.getCurrentToken()) {
+                        refreshTokenHttpFeatureMutex.unlock()
+                        val requestBuilder = HttpRequestBuilder().takeFrom(context.request)
+                        val result: HttpResponse = context.client.request(requestBuilder)
+                        proceedWith(result)
+                    } else {
+                        // If the local current token has not been changed, then will be called
+                        // update callback and repeat the previous request if update was successful
+                        if(feature.updateTokenHandler.invoke()) {
+                            refreshTokenHttpFeatureMutex.unlock()
+                            val requestBuilder = HttpRequestBuilder().takeFrom(context.request)
+                            val result: HttpResponse = context.client.request(requestBuilder)
+                            proceedWith(result)
+                        }
+                        // If the token update wasn't successful
+                        else {
+                            refreshTokenHttpFeatureMutex.unlock()
+                            proceedWith(it)
+                        }
+                    }
                 } else {
                     proceedWith(it)
                 }
