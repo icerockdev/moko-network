5ce396497161b352ed66c16be05c57039af39c60
Alexander Minayev <aminyaev@icerockdev.com>
2020-07-20 18:03:06 +0700

    fix review: param naming and add license

--- network/src/commonMain/kotlin/dev/icerock/moko/network/features/HttpRetrySendFeature.kt
+++ network/src/commonMain/kotlin/dev/icerock/moko/network/features/HttpRetrySendFeature.kt
@@ -1,3 +1,7 @@
+/*
+ * Copyright 2020 IceRock MAG Inc. Use of this source code is governed by the Apache 2.0 license.
+ */
+
 package dev.icerock.moko.network.features
 
 import io.ktor.client.HttpClient
@@ -10,14 +14,14 @@ import io.ktor.utils.io.errors.IOException
 import kotlinx.coroutines.delay
 
 /**
-@param onGetDelay the callback for calculation a delay between failed request and next request
+@param delayGetter the function for calculation a delay between failed request and next request
 @param maxAmountRetrying the max amounts retrying requests
-@param onConditionRetrying the callback for condition a retrying request. By default it check `e is IOException `
+@param isShouldRetryRequest the function for condition a retrying request. By default it check `e is IOException `
  */
 class HttpRetrySendFeature(
-    private val onGetDelay: (lastDelayInMillisecond: Long, timeRetrying: Int) -> Long,
+    private val delayGetter: (lastDelayInMillisecond: Long, timeRetrying: Int) -> Long,
     private val maxAmountsRetrying: Int,
-    private val onConditionRetrying: (e: Throwable) -> Boolean
+    private val isShouldRetryRequest: (e: Throwable) -> Boolean
 ) {
 
     class Config {
@@ -38,17 +42,17 @@ class HttpRetrySendFeature(
             scope.sendPipeline.intercept(HttpSendPipeline.Before) {
                 val counter = context.headers[RETRY_COUNTER_HEADER]?.toInt() ?: 0
                 val lastDelay = context.headers[LAST_DELAY_HEADER]?.toLong()
-                    ?: feature.onGetDelay(0L, counter)
+                    ?: feature.delayGetter(0L, counter)
                 if (counter < feature.maxAmountsRetrying) {
                     try {
                         context.headers.remove(LAST_DELAY_HEADER)
                         context.headers.remove(RETRY_COUNTER_HEADER)
                         proceed()
                     } catch (e: Throwable) {
-                        if (feature.onConditionRetrying(e)) {
+                        if (feature.isShouldRetryRequest(e)) {
                             val requestBuilder = HttpRequestBuilder().takeFrom(context)
                             val indexRetrying = counter + 1
-                            val nextDelay = feature.onGetDelay(lastDelay, counter)
+                            val nextDelay = feature.delayGetter(lastDelay, counter)
                             requestBuilder.headers[RETRY_COUNTER_HEADER] = indexRetrying.toString()
                             requestBuilder.headers[LAST_DELAY_HEADER] = nextDelay.toString()
                             delay(nextDelay)
