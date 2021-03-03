6242039a51e5e6719e24cf73d63c05e7de79a229
tetraquark <avladislavv@gmail.com>
2021-03-03 11:34:26 +0700

    #83 rework ProfileApiTest to fix InvalidMutabilityException with ktor

--- sample/mpp-library/src/commonTest/kotlin/ProfileApiTest.kt
+++ sample/mpp-library/src/commonTest/kotlin/ProfileApiTest.kt
@@ -2,13 +2,19 @@
  * Copyright 2021 IceRock MAG Inc. Use of this source code is governed by the Apache 2.0 license.
  */
 
+import dev.icerock.moko.network.exceptionfactory.HttpExceptionFactory
+import dev.icerock.moko.network.exceptionfactory.parser.ErrorExceptionParser
+import dev.icerock.moko.network.exceptionfactory.parser.ValidationExceptionParser
+import dev.icerock.moko.network.features.ExceptionFeature
 import dev.icerock.moko.network.generated.apis.ProfileApi
 import dev.icerock.moko.network.generated.models.UserInfo
 import dev.icerock.moko.test.runBlocking
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.mock.MockEngine
+import io.ktor.client.engine.mock.MockRequestHandler
 import io.ktor.client.engine.mock.respondError
 import io.ktor.client.engine.mock.respondOk
+import io.ktor.http.ContentType.Application.Json
 import io.ktor.http.HttpStatusCode
 import kotlinx.serialization.builtins.serializer
 import kotlinx.serialization.json.Json
@@ -18,10 +24,7 @@ import kotlin.test.assertNotNull
 import kotlin.test.assertTrue
 
 class ProfileApiTest {
-    private lateinit var httpClient: HttpClient
     private lateinit var json: Json
-
-    private lateinit var profileApi: ProfileApi
 
     private var testResponse: String = ""
     private var isSuccessResponse = false
@@ -31,21 +34,6 @@ class ProfileApiTest {
         json = Json {
             ignoreUnknownKeys = true
         }
-
-        httpClient = HttpClient(MockEngine) {
-            expectSuccess = false
-            engine {
-                addHandler { request ->
-                    if (isSuccessResponse) {
-                        respondOk(content = testResponse)
-                    } else {
-                        respondError(HttpStatusCode.BadRequest)
-                    }
-                }
-            }
-        }
-
-        profileApi = ProfileApi(httpClient = httpClient, json = json)
     }
 
     @Test
@@ -62,6 +50,14 @@ class ProfileApiTest {
                 }
             }
         """.trimIndent()
+        val httpClient = createMockClient {
+            if (isSuccessResponse) {
+                respondOk(content = testResponse)
+            } else {
+                respondError(HttpStatusCode.BadRequest)
+            }
+        }
+        val profileApi = ProfileApi(httpClient = httpClient, json = json)
 
         val result = runBlocking {
             profileApi.profileNotification()
@@ -89,6 +85,16 @@ class ProfileApiTest {
             }
         """.trimIndent()
 
+        val httpClient = createMockClient {
+            if (isSuccessResponse) {
+                respondOk(content = testResponse)
+            } else {
+                respondError(HttpStatusCode.BadRequest)
+            }
+        }
+        val profileApi = ProfileApi(httpClient = httpClient, json = json)
+
+
         val result = runBlocking {
             profileApi.profileNotification()
         }
@@ -99,6 +105,31 @@ class ProfileApiTest {
 
         assertTrue {
             userInfo == "name"
+        }
+    }
+
+    private fun createMockClient(
+        handler: MockRequestHandler
+    ): HttpClient {
+        val json = Json {
+            ignoreUnknownKeys = true
+        }
+
+        return HttpClient(MockEngine) {
+            engine {
+                addHandler(handler)
+            }
+
+            install(ExceptionFeature) {
+                exceptionFactory = HttpExceptionFactory(
+                    defaultParser = ErrorExceptionParser(json),
+                    customParsers = mapOf(
+                        HttpStatusCode.UnprocessableEntity.value to ValidationExceptionParser(json)
+                    )
+                )
+            }
+
+            expectSuccess = false
         }
     }
 }
