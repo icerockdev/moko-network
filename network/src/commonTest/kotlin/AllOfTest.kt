4c6ade53eda5fe7d22a632ed854a48d623d5a749
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2021-03-31 00:47:21 +0700

    #106 allOf serializer

--- network/src/commonTest/kotlin/AllOfTest.kt
+++ network/src/commonTest/kotlin/AllOfTest.kt
@@ -2,28 +2,18 @@
  * Copyright 2021 IceRock MAG Inc. Use of this source code is governed by the Apache 2.0 license.
  */
 
-import kotlinx.serialization.KSerializer
+import dev.icerock.moko.network.schemas.AllOfSerializer
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.descriptors.PrimitiveKind
-import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
-import kotlinx.serialization.descriptors.SerialDescriptor
-import kotlinx.serialization.encoding.Decoder
-import kotlinx.serialization.encoding.Encoder
 import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.JsonDecoder
 import kotlinx.serialization.json.JsonElement
-import kotlinx.serialization.json.JsonEncoder
-import kotlinx.serialization.json.JsonTransformingSerializer
-import kotlinx.serialization.json.buildJsonObject
+import kotlinx.serialization.json.JsonObject
 import kotlinx.serialization.json.jsonObject
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class AllOfTest {
-    private val json = Json {
-        ignoreUnknownKeys = true
-    }
+    private val json = Json.Default
 
     @Test
     fun `allOf decode`() {
@@ -54,93 +44,57 @@ class AllOfTest {
             actual = output
         )
     }
-
-    @Serializable
-    data class Pet(
-        @SerialName("pet_type")
-        val petType: String
-    )
-
-    // allOf
-    @Serializable(with = DogSerializer::class)
-    data class Dog(
-        val pet: Pet,
-        val inlineDog: InlineDog
-    )
-
-    // allOf
-    @Serializable
-    data class Cat(
-        val pet: Pet,
-        val inlineCat: InlineCat
-    )
-
-    @Serializable
-    data class InlineDog(
-        val bark: Boolean? = null,
-        val breed: String? = null
-    )
-
-    @Serializable
-    data class InlineCat(
-        val hunts: Boolean? = null,
-        val age: Int? = null
-    )
-
-    // oneOf / anyOf
-    @Serializable
-    data class CatOrDog(
-        val cat: Cat? = null,
-        val dog: Dog? = null
-    )
 }
 
-object DogJsonSerializer : JsonTransformingSerializer<AllOfTest.Dog>(AllOfTest.Dog.serializer()) {
-    override fun transformDeserialize(element: JsonElement): JsonElement {
-        return buildJsonObject {
-            put("pet", buildJsonObject {
-                element.jsonObject["pet_type"]?.let { put("pet_type", it) }
-            })
-            put("inlineDog", buildJsonObject {
-                element.jsonObject["bark"]?.let { put("bark", it) }
-                element.jsonObject["breed"]?.let { put("breed", it) }
-            })
-        }
-    }
-}
 
-object DogSerializer : KSerializer<AllOfTest.Dog> {
-    override val descriptor: SerialDescriptor =
-        PrimitiveSerialDescriptor("DogSerializer", PrimitiveKind.STRING)
+@Serializable
+private data class Pet(
+    @SerialName("pet_type")
+    val petType: String
+)
 
-    override fun deserialize(decoder: Decoder): AllOfTest.Dog {
-        decoder as JsonDecoder
-        val jsonElement = decoder.decodeSerializableValue(JsonElement.serializer())
-        val pet = decoder.json.decodeFromJsonElement(AllOfTest.Pet.serializer(), jsonElement)
-        val inlineDog =
-            decoder.json.decodeFromJsonElement(AllOfTest.InlineDog.serializer(), jsonElement)
+// allOf
+@Serializable(with = DogSerializer::class)
+private data class Dog(
+    val pet: Pet,
+    val inlineDog: InlineDog
+)
 
-        return AllOfTest.Dog(
+// allOf
+@Serializable
+private data class Cat(
+    val pet: Pet,
+    val inlineCat: InlineCat
+)
+
+@Serializable
+private data class InlineDog(
+    val bark: Boolean? = null,
+    val breed: String? = null
+)
+
+@Serializable
+private data class InlineCat(
+    val hunts: Boolean? = null,
+    val age: Int? = null
+)
+
+private object DogSerializer : AllOfSerializer<Dog>("DogSerializer") {
+
+    override fun decodeJson(json: Json, element: JsonElement): Dog {
+        val pet = json.decodeFromJsonElement(Pet.serializer(), element)
+        val inlineDog = json.decodeFromJsonElement(InlineDog.serializer(), element)
+
+        return Dog(
             pet = pet,
             inlineDog = inlineDog
         )
     }
 
-    override fun serialize(encoder: Encoder, value: AllOfTest.Dog) {
-        encoder as JsonEncoder
-        val petJson = encoder.json.encodeToJsonElement(AllOfTest.Pet.serializer(), value.pet)
-        val inlineDogJson =
-            encoder.json.encodeToJsonElement(AllOfTest.InlineDog.serializer(), value.inlineDog)
+    override fun encodeJson(json: Json, value: Dog): List<JsonObject> {
+        val pet = json.encodeToJsonElement(Pet.serializer(), value.pet).jsonObject
+        val inlineDog = json.encodeToJsonElement(InlineDog.serializer(), value.inlineDog).jsonObject
 
-        val outputObject = buildJsonObject {
-            petJson.jsonObject.entries.forEach {
-                put(it.key, it.value)
-            }
-            inlineDogJson.jsonObject.entries.forEach {
-                put(it.key, it.value)
-            }
-        }
-
-        encoder.encodeJsonElement(outputObject)
+        return listOf(pet, inlineDog)
     }
 }
