9da67b295e5f5d16949b6cee6c45ed0574c02670
Anton Shestak <ashestak@icerockdev.com>
2021-06-17 12:21:38 +0700

    #138 fixes

--- network-generator/settings.gradle.kts
+++ network-generator/settings.gradle.kts
@@ -1,6 +1,7 @@
 /*
  * Copyright 2021 IceRock MAG Inc. Use of this source code is governed by the Apache 2.0 license.
  */
+enableFeaturePreview("VERSION_CATALOGS")
 
 pluginManagement {
     repositories {
