da3159bd0ca026adbd01a374a258fc5ae80ee2d7
Anton Shestak <ashestak@icerockdev.com>
2021-08-27 11:56:21 +0700

    #149 added websocket echo server app

---
+++ sample/websocket-echo-server/src/com/icerockdev/server/Application.kt
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2021 IceRock MAG Inc. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package com.icerockdev.server
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.websocket.*
+import io.ktor.http.cio.websocket.*
+import java.time.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    install(io.ktor.websocket.WebSockets) {
+        pingPeriod = Duration.ofSeconds(15)
+        timeout = Duration.ofSeconds(15)
+        maxFrameSize = Long.MAX_VALUE
+        masking = false
+    }
+
+    routing {
+        get("/") {
+            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+        }
+
+        webSocket("/myws/echo") {
+            send(Frame.Text("Hi from server"))
+            while (true) {
+                val frame = incoming.receive()
+                if (frame is Frame.Text) {
+                    send(Frame.Text("Client said: " + frame.readText()))
+                }
+            }
+        }
+    }
+}
+
