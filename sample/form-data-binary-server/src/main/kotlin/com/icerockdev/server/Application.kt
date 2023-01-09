ed0cb3cc2b91647ac91a4f155c3a3a20ab1886d6
Alexander Kolmachikhin <alexanderkolmachikhin@gmail.com>
2023-01-09 19:37:47 +0700

    #122 fixed code smells

--- sample/form-data-binary-server/src/main/kotlin/com/icerockdev/server/Application.kt
+++ sample/form-data-binary-server/src/main/kotlin/com/icerockdev/server/Application.kt
@@ -4,7 +4,6 @@
 
 package com.icerockdev.server
 
-import io.ktor.http.ContentType
 import io.ktor.http.content.PartData
 import io.ktor.http.content.forEachPart
 import io.ktor.http.content.streamProvider
@@ -12,54 +11,50 @@ import io.ktor.server.application.Application
 import io.ktor.server.application.call
 import io.ktor.server.request.receiveMultipart
 import io.ktor.server.response.respondText
-import io.ktor.server.routing.get
 import io.ktor.server.routing.post
 import io.ktor.server.routing.routing
 import java.io.File
+import kotlin.collections.mutableMapOf
+import kotlin.collections.set
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 fun Application.module() {
     routing {
-        get("/") {
-            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
-        }
-
         post("/v1/auth/signup") {
-            var fileName = ""
             val map = mutableMapOf<String?, String>()
             val multipartData = call.receiveMultipart()
 
             multipartData.forEachPart { part ->
-                when (part) {
-                    is PartData.FormItem -> {
-                        map[part.name] = part.value
-                    }
-
-                    is PartData.FileItem -> {
-                        fileName = part.originalFileName as String
-                        val fileBytes = part.streamProvider().readBytes()
-                        val dir = File("uploads")
-                        dir.mkdir()
-                        File(dir, "$fileName.jpg").writeBytes(fileBytes)
-                    }
-
-                    else -> {}
+                if (part is PartData.FormItem) {
+                    map[part.name] = part.value
                 }
+
+                if (part is PartData.FileItem) {
+                    writePartFile(part)
+                }
+
                 part.dispose()
             }
 
-            val response = """
-                    {
-                        "status": 200,
-                        "message": "avatar uploaded",
-                        "timestamp": 123.0,
-                        "success": true
-                    }
-                """.trimIndent()
-            println("received map: $map and file uploaded to 'uploads/$fileName'")
-            println(response)
-            call.respondText(response)
+            call.respondText(successResponse)
         }
     }
 }
+
+private fun writePartFile(part: PartData.FileItem) {
+    val fileName = part.originalFileName as String
+    val fileBytes = part.streamProvider().readBytes()
+    val dir = File("uploads")
+    dir.mkdir()
+    File(dir, "$fileName.jpg").writeBytes(fileBytes)
+}
+
+val successResponse = """
+    {
+        "status": 200,
+        "message": "avatar uploaded",
+        "timestamp": 123.0,
+        "success": true
+    }
+""".trimIndent()
